8c5e71481d86314e4ce7593a921b38319e6b1e77
Marco Paolo Valerio Vezzoli <marco.p.v.vezzoli@gmail.com>
2024-05-20 22:46:16 +0200

    added iterators

--- src/btree.rs
+++ src/btree.rs
@@ -38,6 +38,14 @@ impl<T> Tree<T> {
             root: Some(node.into()),
         }
     }
+
+    pub fn depth_iter<'a>(& 'a self) -> DepthTraversalIter<'a, T>{
+        DepthTraversalIter::new(self)
+    }
+
+    pub fn breadth_iter<'a>(& 'a self) -> BreadthTraversalIter<'a, T>{
+        BreadthTraversalIter::new(self)
+    }
 }
 
 impl<T: Ord> Tree<T> {
@@ -128,6 +136,7 @@ impl<'a, T> DepthTraversalIter<'a, T> {
     }
 }
 
+
 impl<'a, T> Iterator for DepthTraversalIter<'a, T> {
     type Item = & 'a T;
     fn next(&mut self) -> Option<Self::Item> {
@@ -136,20 +145,31 @@ impl<'a, T> Iterator for DepthTraversalIter<'a, T> {
 }
 
 pub struct BreadthTraversalIter<'a, T>{
-    stack: Vec<& 'a Node<T>>
+    stack: Vec<(usize, & 'a Node<T>)>
+}
+
+impl<'a, T> BreadthTraversalIter<'a, T>{
+    fn new(tree: & 'a Tree<T>) -> BreadthTraversalIter<'a, T>{
+        match tree.root {
+            None => BreadthTraversalIter { stack: Vec::new() },
+            Some(ref node) => BreadthTraversalIter {
+                stack: vec![(0, &node)],
+            },
+        }
+    }
 }
 
 impl<'a, T> Iterator for BreadthTraversalIter<'a, T>{
-    type Item = & 'a T;
+    type Item = (usize, & 'a T);
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some(node) = self.stack.pop() {
+        if let Some((level, node)) = self.stack.pop() {
             if let Some(ref left) = node.left{
-                self.stack.push(& left);
+                self.stack.push((level + 1, & left));
             }
             if let Some(ref right) = node.right{
-                self.stack.push(& right);
+                self.stack.push((level + 1, & right));
             }
-            Some(& node.value)
+            Some((level, & node.value))
         }else{
             None
         }
@@ -169,7 +189,7 @@ mod tests {
         tree.insert(6);
         tree.insert(5);
         println!("{:?}", tree);
-        let result: Vec<i64> = tree.into_iter().map(|x| (*x).clone()).collect();
+        let result: Vec<i64> = tree.depth_iter().map(|x| (*x).clone()).collect();
         assert_eq!(result, vec![4, 5, 6, 8, 10]);
     }
 }
